67f8efa077564cbc6c0034f26a0848d9a93f1db1
Adrian <adrian@rydeas.com>
2022-05-08 21:02:57 -0500

    bumped version made client more generic

--- djstarter/clients.py
+++ djstarter/clients.py
@@ -1,19 +1,18 @@
 import logging
 
-from httpx import Client, Timeout, HTTPStatusError, TransportError
+from httpx import Client, HTTPStatusError, TransportError
 
 from djstarter import decorators
 
 logger = logging.getLogger(__name__)
 
-TIMEOUT = Timeout(connect=5, read=10, write=5, pool=5)
 RETRY_EXCEPTIONS = (
     HTTPStatusError,
     TransportError
 )
 
 
-class Http2Client(Client):
+class RetryClient(Client):
     """
     Http/2 Client
     """
@@ -21,18 +20,9 @@ class Http2Client(Client):
     def __init__(self, *args, **kwargs):
         super().__init__(
             follow_redirects=True,
-            headers=self.init_headers(),
-            http2=True,
-            timeout=TIMEOUT,
             *args,
             **kwargs
         )
-
-    @staticmethod
-    def init_headers():
-        return {
-            'accept': 'application/json',
-        }
 
     @decorators.retry(retry_exceptions=RETRY_EXCEPTIONS)
     @decorators.api_error_check
--- djstarter/tests/test_clients.py
+++ djstarter/tests/test_clients.py
@@ -3,7 +3,7 @@ from unittest import mock
 from django.test import TestCase
 from httpx import HTTPStatusError, NetworkError, Request, Response, codes
 
-from djstarter.clients import Http2Client
+from djstarter.clients import RetryClient
 from djstarter.exceptions import ApiError
 
 
@@ -14,26 +14,26 @@ class Http2ClientTests(TestCase):
         cls.request = Request(url='', method='')  # Must add a non null request to avoid raising Runtime exception
         cls.mocked_sleep = mock.patch('time.sleep', return_value=None).start()
 
-    @mock.patch.object(Http2Client, '_send_handling_auth')
+    @mock.patch.object(RetryClient, '_send_handling_auth')
     def test_ok(self, mock_send):
         mock_send.return_value = Response(
             request=self.request,
             status_code=codes.OK,
             text='ok'
         )
-        with Http2Client() as h_client:
+        with RetryClient() as h_client:
             h_client.get('http://example.com')
             self.assertEquals(mock_send.call_count, 1)
 
-    @mock.patch.object(Http2Client, '_send_handling_auth')
+    @mock.patch.object(RetryClient, '_send_handling_auth')
     def test_retry(self, mock_send):
         mock_send.side_effect = NetworkError(message='')
         with self.assertRaises(NetworkError):
-            with Http2Client() as h_client:
+            with RetryClient() as h_client:
                 h_client.get('https://example.com')
         self.assertEquals(mock_send.call_count, 4)
 
-    @mock.patch.object(Http2Client, '_send_handling_auth')
+    @mock.patch.object(RetryClient, '_send_handling_auth')
     def test_raise_api_error(self, mock_send):
         response = Response(
             request=self.request,
@@ -41,6 +41,6 @@ class Http2ClientTests(TestCase):
         )
         mock_send.side_effect = HTTPStatusError(message='test_123', request=self.request, response=response)
         with self.assertRaises(ApiError):
-            with Http2Client() as h_client:
+            with RetryClient() as h_client:
                 h_client.get('https://example.com')
         self.assertEquals(mock_send.call_count, 1)
